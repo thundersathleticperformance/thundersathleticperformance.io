fc1e80ca3e9da237596e294c43c87b6a898952fe
wearyexcellance <101978004+wearyexcellance@users.noreply.github.com>
2024-07-16 11:06:24 +0100

    Test

--- html5game/vph_canvas_html.js
+++ html5game/vph_canvas_html.js
@@ -208,3 +208,33 @@ function gmh_initiate() {
 		initialised = true;
 	}
 }
+
+function ToggleFullScreen() {
+    if ((document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+        if (document.documentElement.requestFullScreen) {
+            document.documentElement.requestFullScreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullScreen) {
+            document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+    }
+}
+
+
+function allowfs() {
+if (confirm("Fullscreen?") == true) {
+ToggleFullScreen();
+} else {
+}
+}
+
+
